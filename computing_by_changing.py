92cef7d626da564ced91dc4d20bea8da6d6fa606
hoodielive <hoodielive@gmail.com>
2019-02-13 16:12:02 -0500

    ..computing and turing.. almost

---
+++ computing_by_changing.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Turing machines are a imperative model of computation  
+user.email = "aliceinwonderland@example.com"
+user.save() 
+
+
+{
+   "B" , s1 -> X, R, s2
+   "B" , s2 -> B, L, s3
+   "X" , s3 -> B, R, s4
+   "B" , s4 -> B, L, s1
+}
